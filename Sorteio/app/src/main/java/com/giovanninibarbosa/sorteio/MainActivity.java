bc0212a773d09282e596dce04242f50c5b757ffa
Giovannini <giovanninibaarbosa@gmail.com>
2021-01-04 23:00:59 -0300

    Added the second app made that generates a random number.

---
+++ Sorteio/app/src/main/java/com/giovanninibarbosa/sorteio/MainActivity.java
@@ -0,0 +1,24 @@
+package com.giovanninibarbosa.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View v) {
+        TextView textView = findViewById(R.id.numSorteadoMain);
+        int x = new Random().nextInt(10);
+        textView.setText("Número: " + x);
+    }
+}
